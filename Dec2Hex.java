df0501547ffc7cfc14afa6b4408c91b36c8ceae7
oblimode <youcantleavethisempty96@gmail.com>
2024-11-15 11:37:10 +0000

    Fixed a comment issue

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -17,7 +17,7 @@ public static int Arg1;
            System.exit(1);
         }
 
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}; // initialise
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}; // initialise values
         int rem, num;
         num = Arg1;
         String hexadecimal="";
